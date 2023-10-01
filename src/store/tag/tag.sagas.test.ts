3cc024b45dbd5d94889559e6a1b8796264b37881
Juampa Proverbio <juampa.proverbio@themindlab.com>
2023-10-01 19:14:48 +1300

    Environment variables

--- src/store/tag/tag.sagas.test.ts
+++ src/store/tag/tag.sagas.test.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { call } from 'redux-saga/effects';
 import { expectSaga } from 'redux-saga-test-plan';
-import { validateTagApi, TAGS_VALIDATE_PATH } from './tag.api';
+import { validateTagApi, TAGS_VALIDATE_PATH, instance } from './tag.api';
 import {
   ActionTypes,
   validateTagError,
@@ -11,40 +10,36 @@ import {
 } from './tag.actions';
 import { validateTagSaga } from './tag.sagas';
 
-// This sets the mock adapter on the default instance
-const mock = new MockAdapter(axios);
+describe('tagSagas', () => {
+  const mock = new MockAdapter(instance);
 
-it('Validates tag successfully', () => {
-  const mockTagResponse = { message: 'Valid tag' };
+  it('Validates tag successfully', async () => {
+    const mockTagResponse = { message: 'Valid tag' };
+    mock.onPost(TAGS_VALIDATE_PATH).reply(200, mockTagResponse);
 
-  mock.onPost(TAGS_VALIDATE_PATH).reply(200, mockTagResponse);
+    return expectSaga(validateTagSaga, {
+      payload: 'validTag',
+      type: ActionTypes.VALIDATE_TAG,
+    })
+      .put(validateTagLoading())
+      .provide([[call(validateTagApi, 'validTag'), mockTagResponse]])
+      .put(validateTagSuccess(mockTagResponse))
+      .run();
+  });
 
-  return expectSaga(validateTagSaga, {
-    payload: 'validTag',
-    type: ActionTypes.VALIDATE_TAG,
-  })
-    .put(validateTagLoading())
-    .provide([
-      // provide for 'call' effect
-      [call(validateTagApi, 'validTag'), mockTagResponse],
-    ])
-    .put(validateTagSuccess(mockTagResponse))
-    .run();
-});
+  it('Handles error in tag validation', () => {
+    const error = new Error('Unknown error');
+    mock.onPost(TAGS_VALIDATE_PATH).reply(400, error);
 
-it('Handles error in tag validation', () => {
-  const error = new Error('Unknown error');
-
-  mock.onPost(TAGS_VALIDATE_PATH).reply(400, error);
-
-  return expectSaga(validateTagSaga, {
-    payload: 'text is required',
-    type: ActionTypes.VALIDATE_TAG,
-  })
-    .put(validateTagLoading())
-    .provide([
-      [call(validateTagApi, 'text is required'), Promise.reject(error)],
-    ])
-    .put(validateTagError(error.message))
-    .run();
+    return expectSaga(validateTagSaga, {
+      payload: 'text is required',
+      type: ActionTypes.VALIDATE_TAG,
+    })
+      .put(validateTagLoading())
+      .provide([
+        [call(validateTagApi, 'text is required'), Promise.reject(error)],
+      ])
+      .put(validateTagError(error.message))
+      .run();
+  });
 });
